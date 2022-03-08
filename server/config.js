1df4227a6dd4cd1330032a2b80e750a59f508cec
callezenwaka <callisezenwaka@gmail.com>
2022-03-08 11:22:57 +0100

    Add env variables

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
-const marketaddress = process.env.MARKET_ADDRESS;
-const nftaddress = process.env.NFT_ADDRESS;
+const marketaddress = `${process.env.MARKET_ADDRESS}`;
+const nftaddress = `${process.env.NFT_ADDRESS}`;
 
 module.exports = {
 	marketaddress,
